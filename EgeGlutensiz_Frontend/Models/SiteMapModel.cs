4541398cda48fc2c8375cabe586f74fea647b80f
Jenzi <jennydinh94@gmail.com>
2023-04-28 23:38:49 +0200

    Create site map model and components

---
+++ EgeGlutensiz_Frontend/Models/SiteMapModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace EgeGlutensiz_Frontend.Models
+{
+	public class    SiteMapModel
+	{
+        public List<Item> Items { get; set; } = new List<Item>();   
+
+        public class Item
+        {
+            public object originalPageId { get; set; }
+            public string parentId { get; set; }
+            public int sortOrder { get; set; }
+            public string title { get; set; }
+            public object navigationTitle { get; set; }
+            public bool metaIndex { get; set; }
+            public double metaPriority { get; set; }
+            public string menuTitle { get; set; }
+            public string pageTypeName { get; set; }
+            public string permalink { get; set; }
+            public bool isHidden { get; set; }
+            public DateTime published { get; set; }
+            public DateTime created { get; set; }
+            public DateTime lastModified { get; set; }
+            public List<object> permissions { get; set; }
+            public string id { get; set; }
+            public int level { get; set; }
+            public List<Item> items { get; set; }
+        }
+
+    }
+}
+

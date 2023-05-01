dd8efeb11cd7a5a01e9669987c450fc7a373c9d9
Jenzi <jennydinh94@gmail.com>
2023-05-01 10:45:25 +0200

    Create recipesInCategoryModel

---
+++ EgeGlutensiz_Frontend/Models/RecipesInCategoryModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace EgeGlutensiz_Frontend.Models
+{
+	public class RecipesInCategoryModel
+	{
+        public class Root
+        {
+            public object originalPageId { get; set; }
+            public string parentId { get; set; }
+            public int sortOrder { get; set; }
+            public string title { get; set; }
+            public object navigationTitle { get; set; }
+            public bool metaIndex { get; set; }
+            public double metaPriority { get; set; }
+            public string menuTitle { get; set; }
+            public string pageTypeName { get; set; }
+            public string permalink { get; set; }
+            public bool isHidden { get; set; }
+            public DateTime published { get; set; }
+            public DateTime created { get; set; }
+            public DateTime lastModified { get; set; }
+            public List<object> permissions { get; set; }
+            public string id { get; set; }
+            public int level { get; set; }
+            public List<object> items { get; set; }
+        }
+    }
+}
+

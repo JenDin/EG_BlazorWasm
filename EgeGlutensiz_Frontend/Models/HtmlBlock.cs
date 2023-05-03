a8eddce0ba36633327e9b4ca1d838eabe3b06546
Jenzi <jennydinh94@gmail.com>
2023-05-03 13:30:19 +0200

    Change the detail model

---
+++ EgeGlutensiz_Frontend/Models/HtmlBlock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace EgeGlutensiz_Frontend.Models
+{
+	public class HtmlBlock
+	{
+        public class Body
+        {
+            public string value { get; set; }
+        }
+
+            [JsonProperty("$type")]
+            public string type { get; set; }
+            public Body body { get; set; }
+            public string id { get; set; }
+        
+
+
+    }
+}
+
